b26f41334b105110fb832929be6b8a2d48dc93b3
Ashish Kumar Mohan <ashishkumarmohan5@gmail.com>
2025-04-26 12:02:09 +0000

    Created Auth routes and setup controllers for the auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,16 @@
-import express from "express";
-import dotenv from "dotenv";
-
+const express = require("express");
+const dotenv = require("dotenv");
+const {logger} = require("./utils/logger");
+const {apiRoutes} = require("./routes/index");
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/api", apiRoutes);
 const PORT = process.env.PORT;
-app.listen(PORT)
+
+app.listen(PORT, ()=>{
+    logger.info(`App running in port : ${PORT}`);
+});
